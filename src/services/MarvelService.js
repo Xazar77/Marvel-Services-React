282e94bf783c240dc690b8452e6e0207dd15be66
Anderey Prokhorov <beta737@yandex.ru>
2022-02-03 21:17:21 +0300

    Добавление персонажей

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -4,6 +4,7 @@
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/'
     _apiKey = 'apikey=2b41a4868fc7f6cacdf31c2bd6d5e245'
+    _baseOffset = 210
     getResoursce = async (url) => {
         let res = await fetch(url)
 
@@ -14,8 +15,8 @@ class MarvelService {
         return await res.json()
     }
 
-    getAllCharacters = async() => {
-        const res = await this.getResoursce(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`)
+    getAllCharacters = async(offset = this._baseOffset) => {
+        const res = await this.getResoursce(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`)
         return res.data.results.map(this._transformCharacter)
     
     }
@@ -35,11 +36,13 @@ class MarvelService {
             // homepage:  res.data.results[0].urls[0].url,
             // wiki: res.data.results[0].urls[1].url
 
+            id: char.id,
             name: char.name,                            // 2 ВАРИАНТ
             description: char.description,
             thumbnail:  char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage:  char.urls[0].url,
-            wiki: char.urls[1].url
+            wiki: char.urls[1].url,
+            comics: char.comics.items
 
         }
     }
